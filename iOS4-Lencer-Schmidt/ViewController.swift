00f9ba06415e8a88db3f186b2f4f48df19be02af
Timo Schmidt <schmidt@timo.email>
2020-11-25 15:08:40 +0100

    initial

---
+++ iOS4-Lencer-Schmidt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS4-Lencer-Schmidt
+//
+//  Created by Timo Schmidt on 25.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
