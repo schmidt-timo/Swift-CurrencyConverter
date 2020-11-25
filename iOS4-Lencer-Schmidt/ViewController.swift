1c87ef5dfe3fc221f862bf9f684fb4a00f42efce
Timo Schmidt <schmidt@timo.email>
2020-11-25 16:11:23 +0100

    usd field works

--- iOS4-Lencer-Schmidt/ViewController.swift
+++ iOS4-Lencer-Schmidt/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var EURtoUSD: UITextField!
     @IBOutlet weak var EURtoGBP: UITextField!
     
+    @IBOutlet weak var EURfield: UITextField!
+    @IBOutlet weak var USDfield: UITextField!
+    @IBOutlet weak var GBPfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,10 +34,30 @@ class ViewController: UIViewController {
     
     
     @IBAction func USD(_ sender: Any) {
+        
+        let euro_to_usd = OptionalStringToDouble(optional: EURtoUSD.text)
+        
+        let usdollar = OptionalStringToDouble(optional: USDfield.text)
+        
+        // zurück in Euro rechnen
+        let euro: Double = usdollar / euro_to_usd
+        // zurück ins Feld schreiben (als String)
+        EURfield.text = String(format: "%.2lf", euro)
+        
     }
     
     
     @IBAction func GBP(_ sender: Any) {
+    }
+    
+    func OptionalStringToDouble(optional : String?) -> Double {
+        var result = 0.0
+        if let input = optional {
+            if let curr = Double(input) {
+                result = curr
+            }
+        }
+        return result
     }
     
 
